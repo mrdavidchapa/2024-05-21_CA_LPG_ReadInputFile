6acef69cdcab6c50400548a76a812ecfd6ab50eb
mrdavidchapa <mrdavidchapa@gmail.com>
2024-07-04 18:18:52 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,63 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+string line="";
+int intRecordCount=0;
+
+// hello
+
+try
+    {
+       // StreamReader sr = new StreamReader("D:\\TestDataInput.txt");
+        StreamReader sr = new StreamReader("/users/davidchapa/code/textfiles/TestDataInput.txt");
+
+    line = sr.ReadLine();
+
+    while (line != null)
+        {
+   
+           
+
+            var values = line.Split(',');
+
+            /*Console.WriteLine(values.Length);            
+            Console.WriteLine(values[0]);
+            Console.WriteLine(line);
+            */
+
+            int intFieldCount = 0;
+
+            intRecordCount++;
+
+            Console.WriteLine("Record#: {0}",intRecordCount);
+            Console.WriteLine("Field count: {0}", values.Length);
+
+            foreach  ( var item in values) 
+            {
+                intFieldCount++;
+                Console.WriteLine("#: {0} Value: {1}",intFieldCount,item);
+            }
+            Console.WriteLine();
+            Console.WriteLine("*****************************");
+            Console.WriteLine();
+
+            line = sr.ReadLine();
+
+        }
+
+        sr.Close();
+    }
+
+catch (Exception e)
+    {
+        Console.WriteLine("Exception: " + e.Message);
+        Console.WriteLine(line.Length);
+    }
+
+finally
+
+    {
+        Console.WriteLine("Executing finally block");
+    }
+
+∂
